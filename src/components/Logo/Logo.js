9d2e35b0f37a6544c38f7bf616ebec885249eb6e
Dominguez, Hiram <hiramdominguez@my.unt.edu>
2023-03-13 01:10:30 -0500

    Updated logo files

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
+import './Logo.css';
 
 const Logo = () => {
     return (
-    <Tilt>
-        <div style={{ height: '300px'}}>
+    <Tilt className = "br2 shadow-2">
+        <div style={{ height: '100px'}}>
           <h1>🧻Welcome to the Simple Site🧻</h1>
         </div>
     </Tilt>
