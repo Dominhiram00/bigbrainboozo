9ab65ee79563919a790e94a78277cf3a6182872d
Dominguez, Hiram <hiramdominguez@my.unt.edu>
2023-03-13 03:07:21 -0500

    Particles, Rank, and ImageLinkForm added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 function App() {
   return (
     <div className="App">
+      <Particles>
+        params ={{
+          
+        }}
+      </Particles>
       <Navigation/>
       <Logo />
-      {/*
+      <Rank/>
       <ImageLinkForm />
-      <FaceRecognition />*/}
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
