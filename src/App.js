a2a8b7b5100fd1cefb80f2bacf9f6975700ac01b
Hiram Dominguez <dominhiram01@gmail.com>
2023-03-17 22:24:12 -0500

    dsoifjsdf

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import { useCallback } from "react";
+import { Component, useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
-function App() {
-  return (
+class App extends Component {
+  construstor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+  render(){
+   return(
     <div className="App">
       <Particles>
         params ={{
@@ -18,10 +29,11 @@ function App() {
       <Navigation/>
       <Logo />
       <Rank/>
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange = {this.onInputChange}/>
       {/*<FaceRecognition />*/}
     </div>
   );
+      }
 }
 
 export default App;
